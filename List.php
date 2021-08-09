f1c3d0e28758581a4bd972b13bcb37a5067d7345
sunil <sunilbagra108@gmail.com>
2021-08-09 12:07:28 +0530

    list of name and surnames

---
+++ List.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<?php
+$a = array("Sunil"=>"Sharma","Mayank"=>"Mathur","Ravi"=>"Gupta","Amit"=>"Yadav");
+foreach ($a as $a => $value) {
+echo "Name is ".$a." and Surname is ".$value."";
+echo "<br>";}
+?>
+</head>
+</html>
