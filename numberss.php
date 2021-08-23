ea5641b00338b38354caed433ecddfaf3609c3c3
sunilbagra108@gmail.com <sunilbagra108@gmail.com>
2021-08-23 20:56:56 +0530

    marklist of two students

---
+++ numberss.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<?php
+$student_marks = array(
+"Sunil"=> array("English"=> 87, "Maths"=>82),
+"Rahul"=> array("English"=> 94, "Maths"=>72),
+);
+echo "<pre>";
+print_r($student_marks);
+echo "</pre>";
+?>
+</head>
+</html>
