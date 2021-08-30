841938f4eb997b6fe3a76106f3f51bc8eb7fd457
sunilbagra108@gmail.com <sunilbagra108@gmail.com>
2021-08-30 09:55:10 +0530

    information of a author

---
+++ info.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<?php
+$info = array(
+	          "Title" => "The Cuckoos Calling",
+              "Author" => "Robert Galbraith",
+              "Publisher" => "Little Brown"
+               );
+  echo "<pre>";
+  print_r($info);
+  echo "</pre>";
+?>
+</head>
+</html>
