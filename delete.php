508534945bb61e5146f2461dbe5f55f6134c688e
sunilbagra108@gmail.com <sunilbagra108@gmail.com>
2021-08-30 09:31:02 +0530

    delete a element from a array

---
+++ delete.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<?php
+$x = array(1,2,3,4,5);
+var_dump($x);
+ 	echo " ";
+ 	echo "<br>";
+  for($i=1; $i < count($x); $i++) {
+    print_r($key);
+// var_dump($x);
+ 	// echo " ";
+ 	var_dump($x[$i]);
+    echo " ";
+
+ } 
+	/*print_r ($x[$i]);
+	echo  " ";*/
+?>
+</head>
+</html>
