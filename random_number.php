7a9e99bdc33000035309503abe29bb8449afb7ac
sunilbagra108@gmail.com <sunilbagra108@gmail.com>
2021-08-31 12:04:33 +0530

    numbers btw 200 to 250 which is divisible by 4

---
+++ random_number.php
@@ -0,0 +1,16 @@
+<!DOC<!DOCTYPE html>
+<html>
+<body>
+<?php 
+for ($i=200; $i <= 250 ; $i++){
+    if ($i % 4 == 0){
+        print_r($i);
+        echo " ";
+    }
+}   
+?>
+</body>
+</html>
+
+
+
