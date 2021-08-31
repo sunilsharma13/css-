6d624f1879f39b8221bf39d94696bf842191e746
sunilbagra108@gmail.com <sunilbagra108@gmail.com>
2021-08-31 22:42:22 +0530

    largest key

---
+++ largest_key.php
@@ -0,0 +1,11 @@
+ <?php
+$boy = array("Name" => "Rohit" , "Class" => 11 , "Section" => 1);
+// $b = min(array_keys($boy));  
+$a = max(array_keys($boy));
+echo "Largest key in this array is :" .$a. "<br>";
+// echo "Lowest key in this array is: " .$b. "<br>";
+echo $a;
+// echo $b;  
+// print $boy[0];
+?>
+
