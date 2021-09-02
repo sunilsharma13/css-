308fa5f32a7ec4b6092eef990b31bdd622038a84
sunilbagra108@gmail.com <sunilbagra108@gmail.com>
2021-09-02 16:31:54 +0530

    how much times a number appearing

---
+++ pd.php
@@ -0,0 +1,14 @@
+<!-- print into lowercase and uppercase -->
+<?php
+$number = array(34,35,68,84,75,68,57,44,57,45,76);
+// $a = count($number);
+// $b = 68;
+print_r(array_count_values($number));
+// foreach ($number as $value) {
+    // $count = 0;
+    // if($value == 34)
+            // $count++;      
+            // print_r($value);
+            // echo "<br>";
+        // }
+?>
