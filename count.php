d6c677edba75e785f767dcdc1e8afb23ea7328c8
sunilbagra108@gmail.com <sunilbagra108@gmail.com>
2021-09-03 15:26:07 +0530

    program to count how many times a value repeated

---
+++ count.php
@@ -0,0 +1,7 @@
+<?php
+$colors = array(1=>"Green", 2=>"Black", 3=>"Yellow", 4=>"Black");
+// foreach($colors as $key => $values)
+ print_r(array_count_values($colors));
+// echo "\n"."Red color appears ".count_array_values($colors, "Red"). " time(s)."."\n"; 
+// print_r($b);
+?>
